19d766429a92b269d3e80a4421ce4345db802d76
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-02 07:29:07 -0400

    Added function to add item to list

--- custom.js
+++ custom.js
@@ -1,3 +1,20 @@
 const listEl = document.querySelector('ul');
 const userInput = document.querySelector('input');
 const addBtn = document.querySelector('button');
+
+addBtn.addEventListener('click', () => {
+    const listItem = userInput.value;
+    userInput.value = '';
+    
+    const groceryItem = document.createElement('li');
+    const groceryText = document.createElement('span');
+    const removeBtn = document.createElement('button');
+
+    groceryItem.appendChild(groceryText);
+    groceryText.textContent = listItem;
+
+    groceryItem.appendChild(removeBtn);
+    removeBtn.textContent = 'Delete';
+
+    listEl.appendChild(groceryItem);
+});
